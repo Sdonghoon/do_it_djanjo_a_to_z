65a5143c6e9873e9a4511dddd0425d53aeaee64e
20151189 <tls5611@naver.com>
2022-08-11 14:18:07 +0900

    Post 모델에 file_upload 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
