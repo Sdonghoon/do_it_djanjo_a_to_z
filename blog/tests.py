01a9b551f788f14c0901b3c2634ea4d16d0af0a0
20151189 <tls5611@naver.com>
2022-08-12 15:12:48 +0900

    포스트 상세 페이지 테스트하기

--- blog/tests.py
+++ blog/tests.py
@@ -44,3 +44,27 @@ class TestView(TestCase):
 
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+    def test_post_detail(self):
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1')
+
+        response = self.client.get('/blog/1/')
+
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        self.assertIn(post_001.content, post_area.text)
