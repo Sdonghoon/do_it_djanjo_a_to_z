f26b6ce9b71ef8303d6f16eb9819d7d8739a4efe
20151189 <tls5611@naver.com>
2022-08-17 16:44:12 +0900

    포스트 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -204,3 +204,44 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, 'obama')
+
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
+
+        responce = self.client.get(update_post_url)
+        self.assertNotEqual(responce.status_code, 200)
+
+        self.assertNotEqual(self.post_003.author, self.user_trump)
+        self.client.login(
+            username=self.user_trump.username,
+            password='somepassword'
+        )
+        responce = self.client.get(update_post_url)
+        self.assertEqual(responce.status_code, 403)
+
+        self.client.login(
+            username=self.post_003.author.username,
+            password='somepassword'
+        )
+        responce = self.client.get(update_post_url)
+        self.assertEqual(responce.status_code, 200)
+        soup = BeautifulSoup(responce.content, 'html.parser')
+
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        responce = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다.',
+                'content': '안녕 세계? 우리는 하나!',
+                'category': self.category_music.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(responce.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn('안녕 세계? 우리는 하나!', main_area.text)
+        self.assertIn(self.category_music.name, main_area.text)
